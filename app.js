ed0539bf61cc07303c7b0e8a50ee84a34de0de47
Edson Leiva <edsonleiva@Edsons-Mac-mini.attlocal.net>
2022-12-21 22:27:24 -0600

    First Commit App Server Set Up

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(express.static("public"));
+
+app.listen(3000, function(){
+    console.log("Server started on Port 3000");
+});
