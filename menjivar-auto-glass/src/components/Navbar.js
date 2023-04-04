e6f80110fb29f81563a8c5cece51da41e208cd6c
Edson Leiva <edsonleiva@Edsons-Mac-mini.attlocal.net>
2023-04-04 10:05:06 -0500

    Finish Navbar

---
+++ menjivar-auto-glass/src/components/Navbar.js
@@ -0,0 +1,57 @@
+import React, { useEffect, useState, handleClick } from "react";
+import { Link } from "react-router-dom";
+import './Navbar.css'
+
+const Navbar = () => {
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
+
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false)
+  
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  return (
+    <nav className="navbar">
+      <div className="navbar-container">
+        <Link to="/" className="navbar-logo">
+          <img className='logo-size' src={require("./images/Menjivar.png")} alt="logo" />
+        </Link>
+        <div className='menu-icon' onClick={handleClick}> 
+          <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+        </div>
+        <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+          <li className='nav-item'>
+            <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+              Home
+            </Link>
+          </li>
+          <li className='nav-item'>
+            <Link to='/services' className='nav-links' onClick={closeMobileMenu}>
+              Services
+            </Link>
+          </li>
+          <li className='nav-item'>
+            <Link to='/contact' className='nav-links' onClick={closeMobileMenu}>
+              Contact
+            </Link>
+          </li>
+          <li className='nav-item'>
+            <Link to='/about-Us' className='nav-links' onClick={closeMobileMenu}>
+              About Us
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
