ec0ce235e6d37de731dd8f9737251ccc47716816
Edson Leiva <edsonleiva@Edsons-Mac-mini.attlocal.net>
2023-04-14 01:34:18 -0500

    Finish Desktop Layout for Home

--- menjivar-auto-glass/src/components/Button.js
+++ menjivar-auto-glass/src/components/Button.js
@@ -2,9 +2,9 @@ import React from "react";
 import ".Button.css";
 import { Link } from "react-router-dom";
 
-const STYLES = ['btn--primary', 'btn--outline'];
+const STYLES = ["btn--primary", "btn--outline"];
 
-const SIZES = ['btn--medium', 'btn--large'];
+const SIZES = ["btn--medium", "btn--large"];
 
 export const Button = ({
   children,
@@ -13,15 +13,21 @@ export const Button = ({
   buttonStyle,
   buttonSize,
 }) => {
-    const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0]
+  const checkButtonStyle = STYLES.includes(buttonStyle)
+    ? buttonStyle
+    : STYLES[0];
 
-    const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
+  const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
 
-    return (
-        <Link to='/quote' className='btn-mobile'>
-            <button className={'btn ${checkButtonStyle} ${checkButtonSize}'} onClick={onClick} type={type}>
-                {children}
-            </button>
-        </Link>
-    )
+  return (
+    <Link to="/quote" className="btn-mobile">
+      <button
+        className={"btn ${checkButtonStyle} ${checkButtonSize}"}
+        onClick={onClick}
+        type={type}
+      >
+        {children}
+      </button>
+    </Link>
+  );
 };
--- menjivar-auto-glass/src/components/Home.js
+++ menjivar-auto-glass/src/components/Home.js
@@ -1,11 +1,62 @@
-import React from 'react'
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHand } from "@fortawesome/free-solid-svg-icons";
+import "./Home.css";
 
 const Home = () => {
-    return (
-        <div>
-            <h2>Home</h2>
+  return (
+    <div>
+      <img
+        className="banner-img"
+        src={require("./images/test-image03.jpg")}
+        alt="test image"
+      />
+      <div className="container">
+        <h1 id="brand-title">Menjivar Auto Glass</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+        <div className="caution-box">
+        <FontAwesomeIcon icon={faHand} className="caution-icon" />
+        <p className="caution-text">
+          Caution: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+          do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+          ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>            
         </div>
-    )
-}
 
-export default Home
+        <h2>Completed Jobs:</h2>
+        <div className="job-flex">
+            <img
+            className="jobs-completed-img"
+            src={require("./images/job-completed01.jpg")}
+            alt="job-completed-01"
+            />
+            <img
+            className="jobs-completed-img"
+            src={require("./images/job-completed02.jpg")}
+            alt="job-completed-02"
+            />
+            <img
+            className="jobs-completed-img"
+            src={require("./images/job-completed03.jpg")}
+            alt="job-completed-03"
+            />
+        </div>
+        
+      </div>
+    </div>
+  );
+};
+
+export default Home;
